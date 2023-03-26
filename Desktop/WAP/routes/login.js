77153cfa71171194c56a64f866e20b8d66bedc64
hedger9487 <hedger9487@gmail.com>
2023-03-26 13:12:44 +0800

    login.js success by also adding bcrypt in register

--- Desktop/WAP/routes/login.js
+++ Desktop/WAP/routes/login.js
@@ -12,7 +12,6 @@ session.configureSession(router)
 // Login endpoint
 router.post('/', async (req, res) => {
   const { email, password } = req.body
-
   db.getConnection(async (err, connection) => {
     // handle connection errors
     if (err) {
@@ -21,47 +20,41 @@ router.post('/', async (req, res) => {
     }
     // perform database operations
     let sql = 'SELECT * FROM User WHERE email = ?'
-    const rows = await connection.query(sql, [email])
-    if (rows && rows.length > 0) {
-      console.log(rows[0].firstName)
-      console.log(rows[0].lastName)
-      console.log(rows[0].password)
-      console.log(rows[0].userId)
-    } else {
-      console.log('No rows found')
-    }
-    if (rows.length === 0) {
-      return res.status(401).json({ message: 'Invalid email or password' })
-    }
 
-    const match = await bcrypt.compare(password, rows[0].password)
-    if (!match) {
-      return res.status(401).json({ message: 'Invalid email or password' })
-    }
-    // Create session and store user data
-    const user = {
-      userId: rows[0].userId,
-      lastName: rows[0].lastName,
-      firstName: rows[0].firstName,
-      email: rows[0].email,
-    }
-    req.session.user = user
+    await connection.query(sql, [email], (err, results, fields) => {
+      if (err) {
+        console.error(err)
+        return res.status(500).json({ message: 'Server error' })
+      }
+      const user = results[0]
+      if (results.length > 0) {
+        const match = bcrypt.compareSync(password, user.password)
+        if (!match) {
+          return res.status(401).json({ message: 'Invalid email or password' })
+        }
+        // Create session and store user data
+        req.session.user = user
 
-    // Create JWT token
-    const token = session.createToken(user.userId)
+        // Create JWT token
+        const token = session.createToken(user.userId)
 
-    // Set JWT token in cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: false, // set to true if using HTTPS
-      maxAge: 60 * 60 * 1000, // cookie duration in milliseconds
+        // Set JWT token in cookie
+        res.cookie('jwt', token, {
+          httpOnly: true,
+          secure: false, // set to true if using HTTPS
+          maxAge: 60 * 60 * 1000, // cookie duration in milliseconds
+        })
+        res.send('Logged in successfully')
+      } else {
+        console.log('No rows found')
+        return res.status(401).json({ message: 'Invalid email or password' })
+      }
     })
-    res.send('Logged in successfully')
 
     // close connection when done
     connection.release()
     // close connection pool
-    db.closePool()
+    //db.closePool()
   })
 })
 
--- Desktop/WAP/routes/register.js
+++ Desktop/WAP/routes/register.js
@@ -1,38 +1,47 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 const db = require('../public/javascripts/db')
 
 // Register user
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { firstName, lastName, email, password } = req.body
   // validation and database operations
 
-  db.getConnection((err, connection) => {
-    // handle connection errors
-    if (err) {
-      console.error(err)
-      return
-    }
-    // perform database operations
-    let sql =
-      'INSERT INTO User (firstName, lastName, email, password) VALUES (?, ?, ?, ?)'
-    connection.query(
-      sql,
-      [firstName, lastName, email, password],
-      (err, result) => {
-        if (err) {
-          console.error(err)
-        }
-        console.log('1 record inserted')
-        // close connection when done
-        connection.release()
-        // close connection pool
-        // db.closePool()
-      }
-    )
-  })
+  try {
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
 
-  res.send('User registered successfully')
+    db.getConnection((err, connection) => {
+      // handle connection errors
+      if (err) {
+        console.error(err)
+        return
+      }
+      // perform database operations
+      let sql =
+        'INSERT INTO User (firstName, lastName, email, password) VALUES (?, ?, ?, ?)'
+      connection.query(
+        sql,
+        [firstName, lastName, email, hashedPassword],
+        (err, result) => {
+          if (err) {
+            console.error(err)
+          }
+          console.log('1 record inserted')
+          // close connection when done
+          connection.release()
+          // close connection pool
+          // db.closePool()
+        }
+      )
+    })
+
+    res.send('User registered successfully')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Server error')
+  }
 })
 
 module.exports = router
